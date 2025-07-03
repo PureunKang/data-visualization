366d766aeb111ac4268254ea1f07d969fa387922
kangpureun <pureun0000@gmail.com>
2025-07-04 01:30:37 +0900

    feat : 외부 api 페칭 기본 토대

---
+++ src/atoms/covidCountryDataAtom.ts
@@ -0,0 +1,10 @@
+import { atom } from "jotai";
+import { loadable } from "jotai/utils";
+
+const asyncCovidCountryDataAtom = atom(async () => {
+  const res = await fetch("https://disease.sh/v3/covid-19/countries");
+  const data = await res.json();
+  return data;
+});
+
+export const loadableAtom = loadable(asyncCovidCountryDataAtom);
